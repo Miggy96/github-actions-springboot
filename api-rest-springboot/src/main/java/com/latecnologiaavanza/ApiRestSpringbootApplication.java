17c056de430e3bc9f4ce145baf72d82ed9fe2ec6
andres.cruz <andres.cruz@ba.com>
2026-01-28 12:00:39 -0500

    commit codigo

--- api-rest-springboot/src/main/java/com/latecnologiaavanza/ApiRestSpringbootApplication.java
+++ api-rest-springboot/src/main/java/com/latecnologiaavanza/ApiRestSpringbootApplication.java
@@ -24,6 +24,12 @@ public class ApiRestSpringbootApplication {
 		return "Hola desde Spring Boot de nuevo !!";
 	}
 
+	@GetMapping("/saludos")
+	public String saludaraTodos() {
+		return "Hola desde Spring Boot de nuevo, los saludo a todos !!";
+	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestSpringbootApplication.class, args);
 	}
